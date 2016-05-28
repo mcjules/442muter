cf9f1f93638ef82bd162965706627e24fab6189d
mcjules <mcjules@442.com>
2016-05-28 20:34:53 +0930

    Improved usability by allowing the user to mute from the thread page

--- mute.js
+++ mute.js
@@ -1,31 +1,70 @@
 chrome.storage.sync.get({
-  ids: '12345,6789',
-
+    users: []
 }, function(items) {
-  posterIds = items.ids.split(',');
-  mutePosts(posterIds);
+    var posterIds = [];
+    for (var i = 0; i < items.users.length; i++) {
+        posterIds.push(items.users[i].userId);
+    }
+    mutePosts(posterIds);
 });
 
 
-function mutePosts(posterIds){
-var tableRows = $('table[class="content"][cellspacing="1"]').children('tbody').children('tr');
+function mutePosts(posterIds) {
+    var tableRows = $('table[class="content"][cellspacing="1"]').children('tbody').children('tr');
 
-var blockedPost = false;
-for(i =0; i< tableRows.length; i++) {
-  if(blockedPost) {
-    $($(tableRows[i]).children()[0]).html('');
-    $($(tableRows[i]).children()[1]).html('');
-    blockedPost = false;
-  }
-  if($(tableRows[i]).attr('class') == 'postheader') {
-    for (j =0; j< posterIds.length; j++) {
-      var poster = $(tableRows[i]).find('[href="/forums/default.aspx?g=profile&u='+posterIds[j]+'"]');
-      if (poster.length > 0) {
-        poster.parent().html("Muted");
-        $($(tableRows[i]).children()[1]).html('');
-        blockedPost = true;
-      }
+    var blockedPost = false;
+    for (i = 0; i < tableRows.length; i++) {
+
+        if (blockedPost) {
+            //We've checked the header row and it's from a muted user so block the post.
+            $($(tableRows[i]).children()[0]).html('');
+            $($(tableRows[i]).children()[1]).html('');
+            blockedPost = false;
+        }
+        //If it's the header row of a post. Check whether the post is by a muted user.
+        if ($(tableRows[i]).attr('class') == 'postheader') {
+            for (j = 0; j < posterIds.length; j++) {
+                var poster = $(tableRows[i]).find('[href="/forums/default.aspx?g=profile&u=' + posterIds[j] + '"]');
+                if (poster.length > 0) {
+                    poster.parent().html("Muted");
+                    $($(tableRows[i]).children()[1]).html('');
+                    blockedPost = true;
+                }
+
+            }
+            //If it isn't a blocked post, add the mute button next to the username
+            if (!blockedPost) {
+                var usernameTd = $($(tableRows[i]).children()[0]);
+                var username = $(usernameTd.find('a')[1]).text();
+                var link = $(usernameTd.find('a')[1]).attr('href');
+                var userId = link.substring(33, link.length);
+                var html = usernameTd.html();
+                var imgUrl = chrome.extension.getURL("images/music_off.png");
+                usernameTd.html(html + '<a href="javascript:void 0" class="muteUser" alt="Mute this user"><img style="height:20px" src="' + imgUrl + '"></a>');
+            }
+        }
     }
-  }
-}
+    //Register an onclick handler for all the mute buttons
+    $(".muteUser").click(function() {
+        var usernameTd = $(this).parent();
+        var username = $(usernameTd.find('a')[1]).text();
+        var link = $(usernameTd.find('a')[1]).attr('href');
+        var userId = link.substring(33, link.length);
+
+        chrome.storage.sync.get({
+            ids: '',
+            users: []
+
+        }, function(items) {
+            var users = items.users;
+            users.push({
+                'userId': userId,
+                'username': username
+            });
+            items.users = users;
+            chrome.storage.sync.set(items, function() {
+                location.reload();
+            });
+        });
+    });
 }
--- options.js
+++ options.js
@@ -1,28 +1,35 @@
 // Saves options to chrome.storage
 function save_options() {
-  var idValues = document.getElementById('ids').value;
-
-  chrome.storage.sync.set({
-    ids: idValues
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
-  });
+    chrome.storage.sync.set(records, function() {
+        // Update status to let user know options were saved.
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function() {
+            status.textContent = '';
+            location.reload();
+        }, 750);
+    });
 }
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-function restore_options() {
-  chrome.storage.sync.get({
-    ids: '12345,6789',
+function removeUser() {
+    var index = $(this).attr('index');
+    records.users.splice(index, 1);
+    save_options();
+}
+var records = null;
 
-  }, function(items) {
-    document.getElementById('ids').value = items.ids;
-  });
+function restore_options() {
+    chrome.storage.sync.get({
+        ids: '',
+        users: []
+    }, function(items) {
+        records = items;
+
+        for (var i = 0; i < items.users.length; i++) {
+            $('#userBody').append($('<tr>').append($('<td>').text(items.users[i].username)).append($('<td>').text(items.users[i].userId)).append($('<td>').html("<a class='removeLink' href='javascript:void 0' index='" + i + "'>Remove</a>")));
+        }
+        $('.removeLink').click(removeUser);
+    });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',
